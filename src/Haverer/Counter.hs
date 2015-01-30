4853d33d7e2c2e5bd2b2359f9c51b23fee403d0e
Jonathan Lange <jml@mumak.net>
2015-01-30 17:56:43 +0000

    More game logic

--- src/Haverer/Counter.hs
+++ src/Haverer/Counter.hs
@@ -5,13 +5,14 @@ module Haverer.Counter (
   initialize,
   toList,
   top,
+  topValue,
   ) where
 
 import qualified Data.Foldable as Foldable
 import qualified Data.Map as Map
 
 
-data Counter k a = Counter { _uncounter :: Map.Map k a }
+data Counter k a = Counter { _uncounter :: Map.Map k a } deriving Show
 
 incrementMany :: (Ord k, Num a, Foldable.Foldable m) => Counter k a -> m k -> Counter k a
 incrementMany counter keys = Foldable.foldl increment counter keys
--- src/Haverer/Round.hs
+++ src/Haverer/Round.hs
@@ -8,26 +8,34 @@ import Haverer.Player
 
 newtype Score = Score Int deriving (Eq, Ord, Show, Num)
 
-zeroScore :: Score
-zeroScore = Score 0
-
+type PlayerScores = Counter.Counter PlayerId Score
 
 data Game = Game {
+  _winningScore :: Score,
   _players :: Counter.Counter PlayerId Score
-  }
+  } deriving Show
 
 newGame :: PlayerSet -> Game
-newGame ps = Game { _players = (Counter.initialize $ toPlayers ps) }
+newGame ps = Game {
+  _winningScore = Score 4, -- XXX: in some rule sets, this varies based on the number of players
+  _players = (Counter.initialize $ toPlayers ps)
+  }
+
+-- XXX: fclabels
+onCounter :: Game -> (PlayerScores -> PlayerScores) -> Maybe Game
+onCounter game f =
+  if topScoreReached game
+  then Nothing
+  else Just $ game { _players = (f $ _players game) }
 
 newGameForPlayers :: Int -> Maybe Game
 newGameForPlayers = fmap newGame . makePlayerSet
 
-playerWon :: Game -> PlayerId -> Game
-playerWon game player =
-  Game $ Counter.increment (_players game) player
+playerWon :: Game -> PlayerId -> Maybe Game
+playerWon game player = onCounter game (flip Counter.increment player)
 
-playersWon :: Game -> [PlayerId] -> Game
-playersWon game ps = Game $ Counter.incrementMany (_players game) ps
+playersWon :: Game -> [PlayerId] -> Maybe Game
+playersWon game ps = onCounter game (flip Counter.incrementMany ps)
 
 scores :: Game -> [(PlayerId, Score)]
 scores = Counter.toList . _players
@@ -37,3 +45,10 @@ players = fst . unzip . Counter.toList . _players
 
 winning :: Game -> (Score, [PlayerId])
 winning = Counter.top . _players
+
+topScoreReached :: Game -> Bool
+topScoreReached game = Counter.topValue (_players game) >= _winningScore game
+
+winners :: Game -> Maybe [PlayerId]
+winners game = if topScoreReached game then (Just . snd . winning) game else Nothing
+
