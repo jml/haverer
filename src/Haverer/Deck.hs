1469a4b54c91fdd713ed118db2462d8703021bf5
Jonathan Lange <jml@mumak.net>
2015-02-01 11:09:06 +0100

    Somewhat experimental approach to Decks

--- src/Haverer/Deck.hs
+++ src/Haverer/Deck.hs
@@ -1,21 +1,29 @@
 module Haverer.Deck (
   Card(..),
+  Complete,
+  deal,
   Deck,
-  shuffleDeck,
+  Incomplete,
+  makeDeck,
   newDeck,
+  pop,
+  shuffleDeck,
   ) where
 
 
 import Control.Monad
 import Control.Monad.Random (MonadRandom)
-import Data.List (sort)
+import Data.List (sort, (\\))
 import System.Random.Shuffle (shuffleM)
 
 data Card = Soldier | Clown | Knight | Priestess | Wizard | General | Minister | Prince
           deriving (Eq, Show, Ord)
 
 
-newtype Deck = Deck [Card] deriving (Eq, Show, Ord)
+data Complete
+data Incomplete
+
+newtype Deck a = Deck [Card] deriving (Eq, Show, Ord)
 
 baseCards :: [Card]
 baseCards = [
@@ -37,16 +45,29 @@ baseCards = [
   , Prince
   ]
 
-baseDeck :: Deck
+baseDeck :: Deck Complete
 baseDeck = Deck baseCards
 
 
-shuffleDeck :: MonadRandom m => Deck -> m Deck
+shuffleDeck :: MonadRandom m => Deck a -> m (Deck a)
 shuffleDeck (Deck d) = liftM Deck $ shuffleM d
 
-newDeck :: MonadRandom m => m Deck
+newDeck :: MonadRandom m => m (Deck Complete)
 newDeck = shuffleDeck baseDeck
 
-makeDeck :: [Card] -> Maybe Deck
+makeDeck :: [Card] -> Maybe (Deck Complete)
 makeDeck cards =
   if sort cards == baseCards then Just (Deck cards) else Nothing
+
+pop :: Deck a -> (Deck Incomplete, Maybe Card)
+pop (Deck []) = (Deck [], Nothing)
+pop (Deck (c:cards)) = (Deck cards, Just c)
+
+deal :: Deck a -> Int -> (Deck Incomplete, Maybe [Card])
+deal (Deck cards) n =
+  case splitAt n cards of
+   (_, []) -> (Deck cards, Nothing)
+   (top, rest) -> (Deck rest, Just top)
+
+validateSubDeck :: [Card] -> Maybe (Deck Incomplete)
+validateSubDeck cards = if null $ cards \\ baseCards then Just (Deck cards) else Nothing
