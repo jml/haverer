a9275d4b61b4cf2f1dcf1e58366997ee7ad2d559
Jonathan Lange <jml@mumak.net>
2015-02-01 16:17:39 +0100

    Use a pretty printing library

--- Main.hs
+++ Main.hs
@@ -1,4 +1,6 @@
 
+import Text.Show.Pretty
+
 import Haverer.Action
 import Haverer.Deck
 import Haverer.Player
@@ -13,8 +15,8 @@ main = do
   let p1:p2:[] = toPlayers players
   d <- newDeck
   let r = newRound d players
-  putStrLn $ show r
+  putStrLn $ ppShow r
   action <- case playToAction p1 Soldier (Guess p2 Wizard) of
     Left e -> fail (show e)
     Right a -> return a
-  putStrLn $ show $ applyAction r action
+  putStrLn $ ppShow $ applyAction r action
