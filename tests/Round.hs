84259a57479c81929312e99f740d68ed5ac1fb0d
Jonathan Lange <jml@mumak.net>
2015-02-20 23:20:44 +0000

    Move generators to Testing module so we can re-use them in other binaries

--- tests/Round.hs
+++ tests/Round.hs
@@ -19,7 +19,7 @@ module Round where
 
 import Prelude hiding (round)
 
-import Control.Applicative ((<*>), (<$>))
+import Control.Applicative ((<$>))
 import Data.List (isPrefixOf)
 import Data.Maybe (fromJust)
 
@@ -28,8 +28,8 @@ import Test.Tasty.QuickCheck
 
 import Haverer.Action (Play(..), bustingHand, getTarget)
 import Haverer.CommandLine ()
-import Haverer.Deck (baseCards, Card(..), Complete, Deck, makeDeck)
-import Haverer.Player (getHand, getDiscards, isProtected, makePlayerSet, PlayerId, PlayerSet)
+import Haverer.Deck (Card)
+import Haverer.Player (getHand, getDiscards, isProtected, PlayerId)
 import Haverer.Prompt (toText)
 import Haverer.Round (
   Round
@@ -39,7 +39,6 @@ import Haverer.Round (
   , currentTurn
   , getActivePlayers
   , getPlayer
-  , newRound
   , nextPlayer
   , playTurn
   , prop_allCardsPresent
@@ -47,94 +46,11 @@ import Haverer.Round (
   , prop_multipleActivePlayers
   , prop_ringIsActivePlayers
   )
-import Haverer.ValidMoves (attacksOnProtectedPlayers, getValidMoves)
+import Haverer.ValidMoves (attacksOnProtectedPlayers)
 
-import Utils (shuffled, iterateM', isSubListOf)
+import Haverer.Testing
 
-
-instance Arbitrary (Deck Complete) where
-  -- | An arbitrary complete deck is a shuffled set of cards.
-  arbitrary = fmap (fromJust . makeDeck) (shuffled baseCards)
-
-
-instance Arbitrary PlayerSet where
-  -- | Start the game with a random number of players.
-  arbitrary = fmap (fromJust . makePlayerSet) (elements [2, 3, 4])
-
-
-instance Arbitrary Round where
-  -- | A fresh, unplayed round with an arbitrary number of players and a
-  -- shuffled deck.
-  arbitrary = newRound <$> arbitrary <*> arbitrary
-
-
--- | For a Round and a known-good Card and Play, play the cards and return the
--- round and event. If the hand busts out, Card and Play are ignored.
-playTurn' :: Round -> Card -> Play -> (Round, Event)
-playTurn' round card play =
-  case playTurn round of
-   Left (round', event) -> (round', event)
-   Right handlePlay ->
-     case handlePlay card play of
-      Left err -> error $ "Should have generated valid play: " ++ show (err, round, card, play)
-      Right (round', event) -> (round', event)
-
-
-playRandomTurn :: Round -> Gen (Round, Event)
-playRandomTurn round = do
-  move <- randomCardPlay round
-  case move of
-   Nothing -> return (round, RoundOver)
-   Just (card, play) -> return $ playTurn' round card play
-  where
-    randomCardPlay round' =
-      case getValidMoves round' of
-       [] -> return Nothing
-       xs -> elements (fmap Just xs)
-
-
--- | Given a Round, generate a Round that's randomly had a move applied, i.e.
--- a possible next Round. If there are no valid moves, then return the same
--- Round.
-randomNextMove :: Round -> Gen Round
-randomNextMove round = fst <$> playRandomTurn round
-
-
--- | Generate a sequence of N rounds, starting from an initial round.
-manyMoves :: Int -> Gen [Round]
-manyMoves 0 = return []
-manyMoves n = do
-  initial <- arbitrary
-  rest <- iterateM' (n - 2) randomNextMove initial
-  return (initial:rest)
-
-
--- | Generate a random number of consecutive rounds, starting from an initial round.
-randomRounds :: Gen [Round]
-randomRounds = do
-  num <- choose (2, 14)
-  manyMoves num
-
-
--- | Generate a random round that might come up in the course of play.
-randomRound :: Gen Round
-randomRound = last <$> randomRounds
-
-
--- | Generate a random, non-terminating round and a valid play for that round.
-roundAndPlay :: Gen (Round, Card, Play)
-roundAndPlay = do
-  round <- randomRound `suchThat` (not . null . getValidMoves)
-  (card, play) <- elements $ getValidMoves round
-  return (round, card, play)
-
-
--- | Generate an event that might come up in the course of play.
-inRoundEvent :: Gen Event
-inRoundEvent = do
-  (round, card, play) <- roundAndPlay
-  return $ snd $ playTurn' round card play
-
+import Utils (isSubListOf)
 
 -- | It is impossible for the next player to be the current player. That would
 -- mean there's only one person still playing, which would mean that the round
