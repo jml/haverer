cef305ca601fb4fb7bf0f06e4ecd263b4fd3a242
Jonathan Lange <jml@mumak.net>
2015-05-04 00:29:14 +0100

    Nixify

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, containers, lens, MonadRandom, pretty-show
+             , random-shuffle, stdenv, tasty, tasty-hunit, tasty-quickcheck
+             }:
+             mkDerivation {
+               pname = "haverer";
+               version = "0.1.0.0";
+               src = ./.;
+               isLibrary = true;
+               isExecutable = true;
+               buildDepends = [
+                 base containers lens MonadRandom pretty-show random-shuffle tasty
+                 tasty-quickcheck
+               ];
+               testDepends = [
+                 base containers random-shuffle tasty tasty-hunit tasty-quickcheck
+               ];
+               license = stdenv.lib.licenses.asl20;
+             }) {};
+in
+  pkg.env
