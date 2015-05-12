a8e78259c125ea3acf9eb5f9a7f15da8b0bdf1f8
Jonathan Lange <jml@mumak.net>
2015-05-12 09:23:16 +0100

    Instructions for building haverer with Nix
    release.nix is intended to be used with Hydra. This version just builds
    straight from the specified source.

---
+++ release.nix
@@ -0,0 +1,31 @@
+/* Build instructions for Hydra
+ *
+ * Can test these out without Hydra using:
+ *
+ *   $ nix-build release.nix -A build
+ *
+ * Which will build the binary in a local 'result' directory. The binary will
+ * contain executable, documentation, and libraries.
+ */
+
+{ havererSrc ? ./. }:
+
+let
+  pkgs = import <nixpkgs> {};
+
+  jobs = rec {
+
+    build = { system ? builtins.currentSystem }:
+      let
+        pkgs = import <nixpkgs> { inherit system; };
+        modifiedHaskellPackages = pkgs.haskellngPackages.override {
+          overrides = self: super: {
+            haverer = self.callPackage havererSrc;
+          };
+        };
+      in modifiedHaskellPackages.haverer {};
+
+  };
+
+in
+  jobs
