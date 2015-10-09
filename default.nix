27497e8794e38099f60c65fad8f404bf8e9f53a4
Jonathan Lange <jml@mumak.net>
2015-10-09 17:05:35 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,21 @@
-{ mkDerivation, base, basic-prelude, containers, errors, lens, MonadRandom
-, mtl, random-shuffle, stdenv, tasty, tasty-hunit, tasty-quickcheck, text
+{ mkDerivation, base, basic-prelude, containers, errors, lens
+, MonadRandom, mtl, random-shuffle, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, text
 }:
 mkDerivation {
   pname = "haverer";
-  version = "0.1.0.0";
+  version = "0.3.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    base basic-prelude containers errors lens MonadRandom mtl random-shuffle
-    tasty tasty-quickcheck text
+    base basic-prelude containers errors lens MonadRandom mtl
+    random-shuffle tasty tasty-quickcheck text
   ];
   testDepends = [
-    base basic-prelude containers errors mtl random-shuffle tasty tasty-hunit
-    tasty-quickcheck text
+    base basic-prelude containers errors mtl random-shuffle tasty
+    tasty-hunit tasty-quickcheck text
   ];
+  description = "Implementation of the rules of Love Letter";
   license = stdenv.lib.licenses.asl20;
 }
